db0135f8a38830b3846d2520cf341c5441c2f7ba
Ernest <iendeavor.github.io@gmail.com>
2023-02-10 09:17:11 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const importMetaEnv = require('@import-meta-env/unplugin');
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      'template': path.resolve(__dirname, 'public', 'index.html')
+    }),
+
+    importMetaEnv.webpack({
+      example: ".env.example.public",
+    }),
+  ],
+};
